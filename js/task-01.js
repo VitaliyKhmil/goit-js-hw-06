31d5905ce45de61e6a56e956b476cef48fbd0316
VitaliyKhmil <hmilvitaliy@gmail.com>
2022-04-20 21:42:15 +0300

    fix 3dz

--- js/task-01.js
+++ js/task-01.js
@@ -9,28 +9,3 @@ selector.forEach(item => {
     console.log(`Category: ${categoryName}`);
     console.log('Elements: ' + counter);
 });
-
-// const categoryName1 = selector[0].querySelector("h2").textContent;
-// const counter1 = selector[0].querySelectorAll("ul li").length
-// console.log(`Category: ${categoryName1}`);
-// console.log(`Elements: ${counter1}`);
-
-// const categoryName2 = selector[1].querySelector("h2").textContent;
-// const counter2 = selector[1].querySelectorAll("ul li").length
-// console.log(`Category: ${categoryName2}`);
-// console.log(`Elements: ${counter2}`);
-
-// const categoryName3 = selector[2].querySelector("h2").textContent;
-// const counter3 = selector[2].querySelectorAll("ul li").length
-// console.log(`Category: ${categoryName3}`);
-// console.log(`Elements: ${counter3}`);
-
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
--- js/task-05.js
+++ js/task-05.js
@@ -13,15 +13,6 @@ function onInputChange(event){
 }
 
 
-
-
-
-
-
-
-
-
-
 // Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), підставляє його поточне значення в span#name-output. Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
 
 // <input type="text" id="name-input" placeholder="Please enter your name" />
